f13111af1599dfd902dfa95f428d029d8397f022
ygu <yoann.guyot@enspirit.be>
2020-07-23 11:51:04 +0200

    Distinguish between princexml and weasyprint handlers.

--- lib/printit/converter/html2pdf.rb
+++ lib/printit/converter/html2pdf.rb
@@ -1,18 +1,39 @@
 module Printit
   class Html2Pdf
 
-    PRINCE_CONFIG = CONFIG_FILE.load['prince']
+    HANDLER = CONFIG_FILE.load['handler']
 
     def call(html)
-      cmd = "prince #{prince_options} -"
-      result, status = Open3.capture2(cmd, stdin_data: html)
-      result
+      case HANDLER
+      when "prince"
+        convert(html, prince_cmd)
+      when "weasyprint"
+        convert(html, weasyprint_cmd)
+      else
+        raise "Unsupported handler #{HANDLER}."
+      end
     end
 
   private
 
-    def prince_options
-      PRINCE_CONFIG.map{|k,v| "--#{k.gsub('_', '-')}=\"#{v}\"" }.join(" ")
+    def convert(html, cmd)
+      result, status = Open3.capture2(cmd, stdin_data: html, binmode: true)
+      result
+    end
+
+    def prince_cmd
+      options = load_options('prince')
+      "prince #{options} -"
+    end
+
+    def weasyprint_cmd
+      options = load_options('weasyprint')
+      "weasyprint -e utf-8 -f pdf #{options} - -"
+    end
+
+    def load_options(handler)
+      config = CONFIG_FILE.load[handler]
+      options = config.map{|k,v| "--#{k.gsub('_', '-')}=\"#{v}\"" }.join(" ")
     end
 
   end
